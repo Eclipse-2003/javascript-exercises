13b799eeecd90857a6dce4a6f3ae7fc47d9d0314
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-09 23:47:16 -0500

    add comment and refine search to include proper leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,13 +1,11 @@
 const leapYears = function(year) {
-    if (year % 100) {
-        if (year % 400) {
-            return true;
-        }
-    }
-    else if (year % 4) {
+    //check if year is not divisible by 100 or if it is divisible by 400 and 4
+    if ((!(year % 100 === 0) || year % 400 === 0) && year % 4 === 0) {
         return true;
     }
-    return false;
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
