ea898e40085c3d46f3c2535c5da931b1b165a4f5
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-09 22:47:07 -0500

    iterate over args and remove all that dont fit

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) { 
+    let value = 0   
+    for (let i = 0; i < args.length; i++){
+        while (true) {
+            value = arr.findIndex(element => element === args[i])
+            if (value == -1) break;
+            arr.splice(value, 1);
+        } 
+    }
+    return arr;
 };
 
 // Do not edit below this line
