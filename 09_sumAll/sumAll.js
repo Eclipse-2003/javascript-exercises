c956c371a0cd9ae204af725dd7e0a83f5d34abc4
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-09 23:16:42 -0500

    loop through nums and add sum of each values between together while checking for non ints

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    //check each one is an integer
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) return "ERROR";
 
+    if (a > b) {
+        let val = b
+        b = a
+        a = val
+    }
+
+    let sum = 0
+    for (let i = a; i <= b; i++) {
+        sum += i
+    }
+    return sum;
 };
 
 // Do not edit below this line
