30f1b52b84379ea60065f42a775a396c7ceb3b40
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-09 23:58:42 -0500

    fix missing variable and formula error

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -2,8 +2,8 @@ const convertToCelsius = function(num) {
   return Math.round(((num - 32)* (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
-  return Math.round(((num * (5/9) + 32)) * 10) / 10;
+const convertToFahrenheit = function(num) {
+  return Math.round(((num * (9/5) + 32)) * 10) / 10;
 };
 
 // Do not edit below this line
