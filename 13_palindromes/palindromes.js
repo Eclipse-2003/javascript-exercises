7a94da20ebfe1e25b8a915dc969db4fdc924cdec
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-20 20:43:44 -0500

    passed all tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (palin) {
+    function strippedWord (str) {
+        return str
+            .toLowerCase()
+            .replaceAll(' ', '')
+            .replace(/[.,{}[\];':"\\|–=_+()&*%^#$!@`~?\/]/g, '')
+    };
+    let allOneWord = strippedWord(palin);
+    function wordReversal(str) {
+    let reversedWord = '';
+        for (let i = str.length-1; i >= 0; i--) {
+            reversedWord += str[i];
+        }
+        return reversedWord;
+    };
 
+    if (allOneWord === wordReversal(allOneWord)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
