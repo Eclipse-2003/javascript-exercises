6a42b0a285418acd85eea260d4946e86c64467db
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-21 22:12:31 -0500

    check if positive and make str num

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,8 +1,11 @@
 const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
     let veryLast = 0
     let previous = 1;
-    let current = 1;
-    for (let i = 1; i < num; i++) {
+    let current = 0;
+    for (let i = 1; i < Number(num); i++) {
         current = previous + veryLast;
         veryLast = previous;
         previous = current;
