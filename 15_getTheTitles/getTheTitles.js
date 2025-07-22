da9da62bc26719c3ab7ecc95fc5f310a68dcd826
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-21 23:12:47 -0500

    finish function, some room for improvement but works

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let booksArray = [];
+    for (let i = 0; i < array.length; i++) {
+        booksArray.push(array[i].title);
+    }
+    return booksArray;
 };
 
 // Do not edit below this line
