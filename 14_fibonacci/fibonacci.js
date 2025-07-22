b70d1a4822a87f588578bc6cbd877685790c3211
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-21 22:27:16 -0500

    refine code and hard code a solution for 0

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,11 +1,11 @@
 const fibonacci = function(num) {
-    if (num < 0) {
-        return "OOPS";
-    }
+    let number = Number(num);
+    if (number < 0) { return "OOPS"; }
+    if (number == 0) { return 0; }
     let veryLast = 0
     let previous = 1;
-    let current = 0;
-    for (let i = 1; i < Number(num); i++) {
+    let current = 1;
+    for (let i = 1; i < number; i++) {
         current = previous + veryLast;
         veryLast = previous;
         previous = current;
