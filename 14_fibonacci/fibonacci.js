c13d9514628399e974bf4003f7dadcf0db0af176
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-21 22:03:57 -0500

    half way through tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let veryLast = 0
+    let previous = 1;
+    let current = 1;
+    for (let i = 1; i < num; i++) {
+        current = previous + veryLast;
+        veryLast = previous;
+        previous = current;
+    }
+    return current;
 };
 
 // Do not edit below this line
