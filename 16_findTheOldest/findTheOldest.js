9fe3c4da12cce79a305d7dea03298cdfb3e3a44f
Mason McCoy <106836922+Eclipse-2003@users.noreply.github.com>
2025-07-25 20:45:07 -0500

    find who is the oldest from an array and return the object within the array

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const date = new Date();
+    let highestAge = 0;
+    let person = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].yearOfDeath === undefined) {
+            array[i].yearOfDeath = date.getFullYear();
+        }
+        let age = array[i].yearOfDeath - array[i].yearOfBirth;
+        if (age > highestAge) {
+            highestAge = age;
+            person = i;
+        }
+    }
+    return array[person];
 };
 
 // Do not edit below this line
